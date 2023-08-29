bd08862f4bf429655d0e916426c210f4950645a4
chaiyasitKAKA <s6652410025@sau.ac.th>
2023-08-29 10:05:26 +0700

    step1

---
+++ lecture4/test01.py
@@ -0,0 +1,12 @@
+width  = float(input("width:"))
+long = float(input("long:"))
+heigth = float(input("heigth:"))
+cal_font =(width)*(long)
+cal_side =(width)*(heigth)
+cal_top =(heigth)*(long)
+Gallon = round( ((cal_font*2)+(cal_side*2)+(cal_top*2))/5 )
+#print 4 type
+print("จะต้องซื้อ:",Gallon,"gallon")
+print("จะต้องซื้อ:"+""+str(Gallon)+"gallon")
+print(f"จะต้องซื้อ: {Gallon}gallon")
+print("จะต้องซื้อ:{}galon".format(Gallon))
