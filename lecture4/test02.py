fff572dbfae07987fa304090840cacd37a984d4a
chaiyasitKAKA <s6652410025@sau.ac.th>
2023-08-29 10:22:32 +0700

    feet

---
+++ lecture4/test02.py
@@ -0,0 +1,7 @@
+#function 1 - no parameter/No return
+def showHI() :
+    print("HI.....")
+    print("mafxker.....")
+showHI()
+print("hey....")
+showHI() #call function
---
+++ lecture4/test03.py
@@ -0,0 +1,11 @@
+#function 2 -Have parameters/no return
+#parameters คือ ตัวแปร เอาไว้รับค่าทาใช้ในฟังชัน
+
+def funcA(x,y) :
+    print(f"X is {x}")
+    print(f"Y is {y}")
+    print(f"SUM {x} + {y} = {x+y}")
+
+funcA(45, 42) #call function
+funcA(43, 5)
+funcA(3, 35)
