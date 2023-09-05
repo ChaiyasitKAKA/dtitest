ac36085aff6b7e40f4b7ee51e142c645cc3bfbe7
ChaiyasitKAKA <142291242+ChaiyasitKAKA@users.noreply.github.com>
2023-09-05 17:59:29 +0700

    dtipython07.py

--- lecture5/dtipython07.py
+++ lecture5/dtipython07.py
@@ -4,37 +4,47 @@
 import math
 #inputradius
 def inputradius() :
-    r = input("ป้อนรัศมี :")
+    #radius = input("ป้อนรัศมี :")
     #return r
     #r = float(input("ป้อนรัศมี :"))
     #return r
     #return input("ป้อนรัศมี :") 
-    #return float(input("ป้อนรัศมี :"))
-    return r
+    return float(input("ป้อนรัศมี :"))
+    #return r
 
 #calAreaCircle
 def calAreaCircle( r ) :
     area = math.pi * r ** 2  
     # area = math.pi * r * r  
     # area = math.pi * math.pow(r, 2)  
-    # return math.pi * math.pow(r, 2)
-    return area
+    return math.pi * math.pow(r, 2)
+    #return area
 
 
 #calRoundCircle 2 * PI * r
 def calRoundCircle (r) : 
-    round = 2 * math.pi * r
-    return round
+    return 2 * math.pi * r
+    
+
 #calCubeCircle 4 / 3 * PI * r * r * r 
 def  calCubeCircle (r) :
-    cube = 4/3 * math.pi * r * r * r 
-    return cube
+    return 4/3 * math.pi * r * r * r 
+    
 
 #showResult ทศนิยม 4 ตำแหน่ง
 #พื้นที่วงกลมเป็น .... เส้นรอบวงเป็น .... ปริมาตรทรงกลมเป็น
-print(f"พื้นที่วงกลม {area} เส้นรอยวงเป็น {round} ปรืมาตรทรงกลมเป็น {cube} ")
+def showresualt():
+    radius = inputradius()
+    area = calAreaCircle(radius)
+    parameter = calRoundCircle(radius)
+    volume = calCubeCircle(radius)
+    print("Radius","%.4f" % radius)
+    print("Area","%.4f" % area)
+    print("Paramwter","%.4f" % parameter)
+    print("Volume","%.4f" % volume)
 
-inputradius()
+showresualt()
+
 
 
  
