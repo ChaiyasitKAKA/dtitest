a26f3f672ad23448906e9ddf58ed64de46a35766
ChaiyasitKAKA <s6652410025@sau.ac.th>
2023-09-19 14:13:08 +0700

    o3

--- workshopA/workshop04.py
+++ workshopA/workshop04.py
@@ -1,13 +1,13 @@
 # Define a function to calculate y based on x
-def calculate(x):
+def calculateeqution(x):
     y = 2 * x**2 + 2 * x + 1
     return y
 
-# Input the value of x from the user
 x = float(input("Enter the value of x: "))
 
 # Calculate y using the function
-result_y = calculate_y(x)
+
+slove_y = calculateeqution(x)
 
 # Display the calculated y value
-print("The value of y is:", result_y)
+print("The value of y is:", slove_y)
