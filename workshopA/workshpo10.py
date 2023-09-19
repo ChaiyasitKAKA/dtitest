89f9d0c587df9bb80f0dbae4edf8e2ed2ca619cc
ChaiyasitKAKA <s6652410025@sau.ac.th>
2023-09-19 22:40:22 +0700

    10/13

--- workshopA/workshpo10.py
+++ workshopA/workshpo10.py
@@ -1,1 +1,27 @@
-#10. จงเขียนโปรแกรมPython ของโปรแกรมแสดงข้อความต้อนรับนักศึกษา โดยรับชั้นปีทางแป้นพิมพ์แล้วแสดงข้อความต้อนรับ ดังนี้กรณีป้อน 1 แสดงข้อความ "Welcome Freshman"กรณีป้อน 2 แสดงข้อความ "Welcome Sophomore"กรณีป้อน 3 แสดงข้อความ "Welcome Junior"กรณีป้อน 4 แสดงข้อความ "Welcome Senior"
+#10. จงเขียนโปรแกรมPython ของโปรแกรมแสดงข้อความต้อนรับนักศึกษา โดยรับชั้นปีทางแป้นพิมพ์แล้วแสดงข้อความต้อนรับ ดังนี้กรณีป้อน 1 แสดงข้อความ "Welcome Freshman"กรณีป้อน 2 แสดงข้อความ "Welcome Sophomore"กรน ณีป้อ3 แสดงข้อความ "Welcome Junior"กรณีป้อน 4 แสดงข้อความ "Welcome Senior"
+def Inputyearclass():
+    return int(input("Enter your year class 1-4:"))
+    
+def checkyear(year):
+    if year == 1:
+        return  "Welcome Freshman"
+    elif year == 2:
+        return  "Welcome Sophomore"
+    elif year == 3:
+        return  "Welcome Junior"
+    elif year == 4:
+        return  "Welcome Senior"
+    else:
+        return "Invalid year. Please enter a year between 1 and 4."
+
+
+def showResault(message): 
+    print("-----------------------------")
+    print("        WELCOME MESSAGE      ")
+    print("-----------------------------")
+    print(f"       {message}            ")
+
+
+year =  Inputyearclass()
+message = checkyear(year)
+showResault(message)
