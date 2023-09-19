031bf1b53137fc45cd8590b6027a114f481ef954
chaiyasitKAKA <s6652410025@sau.ac.th>
2023-09-19 11:48:41 +0700

    00

--- workshopA/workshop03.py
+++ workshopA/workshop03.py
@@ -6,6 +6,10 @@ def inputproductnameAndprice():
 def caltaxAndshow(productname,price) :
     pay = price - (price * 7/100)
     print(f"Productname: {productname} Tax: {pay} ")
-
+print("--------------------------")
+print("       TAXcalculator      ")
+print("--------------------------")
 productname,price=inputproductnameAndprice()
 caltaxAndshow(productname,price)
+
+
