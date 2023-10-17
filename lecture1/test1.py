ce2542fffc7cb623d3a289da180ff86d1400f7a7
chaiyasitKAKA <s6652410025@sau.ac.th>
2023-10-17 12:16:14 +0700

    OOP

--- lecture1/test1.py
+++ lecture1/test1.py
@@ -1,10 +1,20 @@
-print("hi")
-print("""
-We are the lion king
-""")
-user_name =input("username:")
-user_type =input("animaltype:")
-print("wellcome","the",(user_type),(user_name), "to the junger")
-print("wellcome"+"the"+(user_type)+(user_name)+" "+"to the junger")
-print(f"wellcome {user_type} {user_name} to the junger")
-print("wellcome the {} {} to the junger ".format(user_type,user_name))
+# print("hi")
+# print("""
+# We are the lion king
+# """)
+# user_name =input("username:")
+# user_type =input("animaltype:")
+# print("wellcome","the",(user_type),(user_name), "to the junger")
+# print("wellcome"+"the"+(user_type)+(user_name)+" "+"to the junger")
+# print(f"wellcome {user_type} {user_name} to the junger")
+# print("wellcome the {} {} to the junger ".format(user_type,user_name))
+
+def input_smt() : 
+    return input("Enter Something:")
+  
+
+def change_som(smt) :
+    smt = print("DTI")
+
+smt = input_smt()
+change_som(smt)
